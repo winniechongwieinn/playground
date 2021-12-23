c6999ef485092943b3444132b3bab4b94759e229
winniechong <winnie_wieinn@yahoo.com>
2021-12-23 15:24:24 +0800

    fixed form submission

--- frontend/src/components/common/contact-form/index.js
+++ frontend/src/components/common/contact-form/index.js
@@ -5,8 +5,10 @@ import './index.scss';
 export const ContactForm = () => {
     const [status, setStatus] = useState("Submit");
 
-    const encodeData = (data) => {
-        return Object.keys(data).map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])).join("&");
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&")
     }
 
     const handleSubmit = async (e) => {
@@ -24,29 +26,30 @@ export const ContactForm = () => {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
             },
-            body: encodeData({ "form-name": "contactForm", ...details }),
+            body: encode(details),
         });
         setStatus("Submit");
         let result = await response.json();
         alert(result.status);
     };
     return (
-        <form method="post" className="contact-form" name="contactForm" data-netlify="true" onSubmit={handleSubmit} >
+        <form className="contact-form" onSubmit={handleSubmit} >
+            <input type="hidden" name="form-name" value="contactForm" />
             <div>
                 <label htmlFor="firstname">First Name<span>*</span></label>
-                <input type="text" id="firstname" required />
+                <input type="text" id="firstname" name="firstname" required />
             </div>
             <div>
                 <label htmlFor="lastname">Last Name<span>*</span></label>
-                <input type="text" id="lastname" required />
+                <input type="text" id="lastname" name="lastname" required />
             </div>
             <div>
                 <label htmlFor="email">Email<span>*</span></label>
-                <input type="email" id="email" required />
+                <input type="email" id="email" name="email" required />
             </div>
             <div>
                 <label htmlFor="message">Message<span>*</span></label>
-                <textarea id="message" required />
+                <textarea id="message" name="message" required />
             </div>
             <button type="submit">{status}</button>
         </form>
